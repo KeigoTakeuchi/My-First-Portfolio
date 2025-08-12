796c3e61354efcb19205fbfd40321070ad27518e
KeigoT <keigo20000904@gmail.com>
2025-08-12 13:39:34 +0900

    fix: Entityクラスのアノテーション修正

--- backend/src/main/java/com/example/demo/entity/Image.java
+++ backend/src/main/java/com/example/demo/entity/Image.java
@@ -2,9 +2,13 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Image {
 	
 	private int id;
--- backend/src/main/java/com/example/demo/entity/Message.java
+++ backend/src/main/java/com/example/demo/entity/Message.java
@@ -3,9 +3,13 @@ package com.example.demo.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
 
 	private int id;
