ec3bbef57d35f9aaaa1ccea33cff9b32f242d1d0
KeigoT <keigo20000904@gmail.com>
2025-08-25 14:05:33 +0900

    feat:MessageServiceImplの実装
    Message投稿に関連したServiceImplの実装。画像投稿やAccountとの関連(連鎖)処理は未実装

--- backend/src/main/java/com/example/demo/service/Impl/MessageServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/Impl/MessageServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.service.Impl;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,9 +7,13 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.MessageFormDTO;
+import com.example.demo.dto.MessageViewDTO;
 import com.example.demo.entity.Account;
 import com.example.demo.entity.Message;
+import com.example.demo.helper.DTOConverter;
 import com.example.demo.repository.AccountMapper;
+import com.example.demo.repository.ImageMapper;
 import com.example.demo.repository.MessageMapper;
 import com.example.demo.service.MessageService;
 
@@ -23,27 +26,49 @@ public class MessageServiceImpl implements MessageService {
 
 	private final MessageMapper messageMapper;
 	private final AccountMapper accountMapper;
+	private final ImageMapper imageMapper;
 	
-	public List<Message>  getAllMessages(){
+	public List<MessageViewDTO> getAllMessages(){
 		
 		List<Message> messages = messageMapper.getAllMessages();
 		
 		if(messages.isEmpty()) {
-			return new ArrayList<Message>(); 
+			return new ArrayList<MessageViewDTO>(); 
 		}
-		return messages;
+		
+		List<MessageViewDTO> messageViewList = new ArrayList<MessageViewDTO>();
+		
+		for(Message message : messages) {
+			
+			messageViewList.add(DTOConverter.convertToMessageViewDTO(message));
+		}
+		return messageViewList;
 	}
 	
-	public List<Message> getAllMessagesByAccountId(Integer accountId){
+	public List<MessageViewDTO> getAllMessagesByAccountId(Integer accountId){
 		List<Message> messages = messageMapper.getAllMessagesByAccountId(accountId);
 		
 		if (messages.isEmpty()) {
-			return new ArrayList<Message>();
+			return new ArrayList<MessageViewDTO>();
 		}
-		return messages;
+		
+		List<MessageViewDTO> messageViewList = new ArrayList<>();
+		
+		for (Message message : messages) {
+			
+			messageViewList.add(DTOConverter.convertToMessageViewDTO(message));
+		}
+		return messageViewList;
 	}
 	
-	public List<Message> searchMessagesByWord(String word){
+	//Messageの詳細画面にはAccount,ImageのDTO情報をもつ
+	public MessageViewDTO getMessage(Integer id) {
+		Message message = messageMapper.getMessageById(id);
+		MessageViewDTO messageViewDTO = DTOConverter.convertToMessageViewDTO(message);
+		return messageViewDTO;
+	}
+	
+	public List<MessageViewDTO> searchMessagesByWord(String word){
 		
 		if (word == null || word.trim().isEmpty()) {
 			return Collections.emptyList();
@@ -51,34 +76,64 @@ public class MessageServiceImpl implements MessageService {
 		
 		String searchQuery = "%" + word + "%";
 		
-		List<Message> searchLists = messageMapper.getMessagesByWord(searchQuery);
-		return searchLists;
-	}
-	
-	public void postMessage(Message message,Integer accountId) {
-		Account account = accountMapper.getAccountById(accountId);
-		//MessageはDTOに替えたのち、下で補完してあげる必要あり
-		message.setAccount(account);
-		message.setAccountId(account.getId());
-		messageMapper.insertMessage(message);
-	}
-	
-	public void changeTitle(Message message) {
+		List<Message> searchList = messageMapper.getMessagesByWord(searchQuery);
 		
-		messageMapper.updateTitleById(message);
+		List<MessageViewDTO> messageViewList = new ArrayList<>();
+		
+		for(Message message : searchList) {
+			MessageViewDTO messageViewDTO = DTOConverter.convertToMessageViewDTO(message);
+			messageViewList.add(messageViewDTO);
+		}
+		return messageViewList;
 	}
 	
-	public void changeContent(Message message) {
-		messageMapper.updateContentById(message);
+	//Valid通過後のハンドラメソッド内でScopeからAccountIdを取得する
+	public void postMessage(MessageFormDTO messageForm,Integer accountId) {
+		//引数のIDをもとにAccountEntityの取得
+		Account account = accountMapper.getAccountById(accountId);
+				
+		//Formから画像を受け取った場合の処理
+		if(messageForm.getImages() != null && !messageForm.getImages().isEmpty()) {
+			/*ここでMultipartFile型のDTOをUUIDで自動生成した文字列とファイル名(MultipartFile.getOriginalFileName()で取得可)を
+			 * 組み合わせたオブジェクトキーを紐づけてfilePathプロパティに格納(例:/images/user/afhd2442jlnj24k.jpg)
+			 * して、MessageForm内のList<MultipartFile> imagesをImageEntityとして補完していき、完成したImageEntityのListを保持しておく
+			 */
+		}
+		//取得したAccountEntityとFormから受け取った変数をもとにMessageEntityへ変換(Imageは別で変換させる)
+		Message message = DTOConverter.convertToMessage(messageForm, account);
+
+		messageMapper.insertMessage(message.getAccount().getId(),message);
+		
+		//上で作成したList型ImageEntityからImageEntity抜き出してDB保存(ここではList型をimagesとして仮作成)
+		/*for (Image image : images) {
+			imageMapper.insertImage(message.getId(),image);
+		}
+		*/
+		
+	}
+	/*ControllerでFormページで送られてきたFormDTOとmessageID(@PAthVariable)をもとにMessageEntityを完成させて
+	 * MessageMapperに渡してあげる
+	 */
+	public void changeMessage(MessageFormDTO messageForm,Integer id) {
+		//選択されたmessageIdをもとにEntityの準備
+		Message message = messageMapper.getMessageById(id);
+		
+		//Formから受け取った値をEntityにset
+		message.setTitle(messageForm.getInputTitle());
+		message.setContent(messageForm.getInputContent());
+		
+		//EntityをMapper更新メソッドに渡す
+		messageMapper.updateMessageById(message);
 	}
 	
-	public void deleteMessages(Message message,Integer id) {
-		LocalDateTime updatedAt = message.getUpdatedAt();
-		messageMapper.deleteAllMessagesByAccountId(id, updatedAt);
+	
+	//一覧表示から消すことを想定
+	public void deleteMessages(Integer id) {
+		Message message = messageMapper.getMessageById(id);
+		messageMapper.deleteMessageById(id, message.getUpdatedAt());
 	}
 	
-	public void deleteAllMessagesByAccount(Message message,Integer accountId) {
-		LocalDateTime updatedAt = message.getUpdatedAt();
-		messageMapper.deleteAllMessagesByAccountId(accountId,updatedAt);
+	public void deleteAllMessagesByAccount(Integer accountId) {
+		messageMapper.deleteAllMessagesByAccountId(accountId);
 	}
 }
